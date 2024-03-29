340a6df3954dc44e11facbc545e154990dfa682f
terakado <kenji.terakado@gmail.com>
2024-03-29 12:11:57 +0900

    add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,58 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World !!");
+    }
+
+	public static void sub (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub10 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub20 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+	public static void sub11 (void) {
+		System.out.println("Hello sub !!");
+	}
+
+        public static void sub30 (void) {
+		System.out.println("Hello terakado !!");
+	}
+}
